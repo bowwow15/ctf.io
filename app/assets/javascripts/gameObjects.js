e259104eb177b8ad11a68749e2728498e23e387b
= <chipwibb@gmail.com>
2018-03-07 19:00:24 -0800

    moved keystroke boolean variables to gameObjects.ks

--- app/assets/javascripts/gameObjects.js
+++ app/assets/javascripts/gameObjects.js
@@ -10,3 +10,82 @@ var ServerGameObject = {
 		ctx.fill();
 	}
 };
+
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+//event listener
+window.addEventListener("keydown", onKeyDown, false);
+window.addEventListener("keyup", onKeyUp, false);
+
+function onKeyDown(event) {
+  var keyCode = event.keyCode;
+  switch (keyCode) {
+    case 68: //d
+      keyD = true;
+      break;
+    case 83: //s
+      keyS = true;
+      break;
+    case 65: //a
+      keyA = true;
+      break;
+    case 87: //w
+      keyW = true;
+      break;
+    case 16: //shift key (sprint)
+      keyShift = true;
+      break;
+    case 67: //b
+      keyC = true;
+      break;
+  }
+
+  if (keyC == true) {
+				if (Player.color != true) {
+					Player.color = true;
+				}
+				else {
+					Player.color = false;
+				}
+			}
+	}
+
+function onKeyUp(event) {
+  var keyCode = event.keyCode;
+
+  switch (keyCode) {
+    case 68: //d
+      keyD = false;
+      break;
+    case 83: //s
+      keyS = false;
+      break;
+    case 65: //a
+      keyA = false;
+      break;
+    case 87: //w
+      keyW = false;
+      break;
+    case 16: //shift key (sprint)
+      keyShift = false;
+      break;
+    case 67: //b
+      keyC = false;
+      break;
+  }
+}
+
+//neccessary variables
+var tickX = 10;
+var tickY = 10;
+
+var keyW = false;
+var keyA = false;
+var keyS = false;
+var keyD = false;
+var keyShift = false;
+var keyC = false;
